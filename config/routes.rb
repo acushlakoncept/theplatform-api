877d57697a1404d69188afcc045c8ffc7a0876a4
Uduak Essien <acushla4real@gmail.com>
2023-05-06 14:52:23 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  
+  namespace :api do
+    namespace :v1 do
+      post 'login', to: 'authentication#create'
+
+      resources :users, only: %i[show create] do
+        member do
+          get :confirm_email
+        end
+      end
+    end
+  end
 end
