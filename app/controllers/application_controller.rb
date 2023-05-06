2fd82b89a51e30811c72f25436d6102d5579fea5
Uduak Essien <acushla4real@gmail.com>
2023-05-06 05:48:49 +0100

    Add auth request and current_user! to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  def authenticate_request!
+    return invalid_authentication if !payload || !AuthenticationTokenService.valid_payload(payload.first)
+
+    current_user!
+    invalid_authentication unless @current_user
+  end
+
+  def current_user!
+    @current_user = User.find_by(id: payload[0]['user_id'])
+  end
+
+  def invalid_authentication
+    render json: { error: 'You will need to login first' }, status: :unauthorized
+  end
+
+  private
+
+  def payload
+    auth_header = request.headers['Authorization']
+    pp auth_header
+    token = auth_header.split(' ').last
+    AuthenticationTokenService.decode token
+  rescue StandardError
+    nil
+  end
 end
