954d1a7146f7a292795690bd5847bb714c47f937
Uduak Essien <acushla4real@gmail.com>
2023-05-23 08:45:57 +0100

    Update routes for confirm_email

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :role, :full_name, :email, :phone, :token, :referral_url, :username,
+  attributes :id, :role, :full_name, :email, :phone, :token,
              :pronouns, :bio, :skills, :language, :photo, :email_confirmed, :photo
 
-  def full_name
-    object.full_name
-  end
 
   def token
     AuthenticationTokenService.call(object.id)
